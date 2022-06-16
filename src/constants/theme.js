73deba2a65a24a511b074e9986c007f0a6df9ca6
CODE PICASSO <thecodepicasso@gmail.com>
2022-06-16 15:43:31 -0700

    Intialised Project, Setup project Structure, Installed Dependecies

---
+++ src/constants/theme.js
@@ -0,0 +1,21 @@
+import { Dimensions } from 'react-native';
+export const windowWidth = Dimensions.get('window').width;
+export const windowHeight = Dimensions.get('window').height;
+
+export const Colors = {
+	primary: '#1E4B6C',
+	white: '#fff',
+	grey: '#333',
+	danger: '#DC143C',
+	go: '#8ac926',
+	black: '#000',
+	ashes: '#cecece',
+};
+
+export const Sizes = {
+	padding: 30,
+	borderRadiu: 50,
+	border: 1,
+	font: 16,
+	icon: 24,
+};
